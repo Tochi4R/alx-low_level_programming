fc2af836e04bd9dd26edf5efbe5dc35f346c97aa
Samuel_tochukwu <tochukwu_samuel@yahoo.com>
2023-02-17 10:21:47 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * main - Entry
+ * Desciption: Write a C program with puts
+ * Return: always 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle".\n);
+	return (0);
+}
