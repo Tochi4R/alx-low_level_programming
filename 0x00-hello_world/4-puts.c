7669a6c246a334c91735d84dccc4ca5c3fa1089e
Samuel_tochukwu <tochukwu_samuel@yahoo.com>
2023-02-17 10:43:57 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 /**
  * main - Entry
  * Desciption: Write a C program with puts
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle".\n);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
