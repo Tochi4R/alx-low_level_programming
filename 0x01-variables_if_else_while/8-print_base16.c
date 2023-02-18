559e16233bc697d3c4ae187f201b574f4507da40
Samuel_tochukwu <tochukwu_samuel@yahoo.com>
2023-02-18 15:49:56 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,13 +11,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
 	}
-	for (i = 0; i <= 16; i++)
+	for (i = 10; i < 16; i++)
 	{
-		putchar(i + 10 + 'a');
+		putchar(i - 10 + 'a');
 	}
 	putchar('\n');
 	return (0);
