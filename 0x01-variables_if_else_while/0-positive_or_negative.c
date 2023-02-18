3c10917231d2aae507b0da739d7fcdeee721b740
Samuel_tochukwu <tochukwu_samuel@yahoo.com>
2023-02-18 12:49:47 -0800

    Positive anything is better than negative nothing

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point for the program
+ *
+ * Description: This function is the entry point for the program and
+ *              is responsible for running the program's main logic.
+ *
+ * Return:      Returns an integer indicating the exit status of the program.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf(" %d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
+}
