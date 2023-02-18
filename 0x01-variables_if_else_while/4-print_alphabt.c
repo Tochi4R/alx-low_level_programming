570e96ca2e4fb725f809a54fdafceef54f683205
Samuel_tochukwu <tochukwu_samuel@yahoo.com>
2023-02-18 15:17:56 -0800

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Description: This function is the entry point for the program and
+ *              is responsible for running the program's main logic.
+ *
+ * Return:      Returns an integer indicating the exit status of the program
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	putchar('\n');
+	return (0);
+}
