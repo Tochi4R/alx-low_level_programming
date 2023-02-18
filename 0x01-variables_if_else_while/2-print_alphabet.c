a3bc7a9bee36387911b3e5497cb349cda2f59a32
Samuel_tochukwu <tochukwu_samuel@yahoo.com>
2023-02-18 14:50:09 -0800

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Description: This function is the entry point for the program and
+ *              is responsible for running the program's main logic.
+ *
+ * Return:      Returns an integer indicating the exit status of the program
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n);
+}
