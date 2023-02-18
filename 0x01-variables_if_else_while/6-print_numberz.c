309513498bce3c6d42c8dc2c7dfb40a286b3f8bf
Samuel_tochukwu <tochukwu_samuel@yahoo.com>
2023-02-18 15:26:06 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Description: This function is the entry point for the program and
+ *              is responsible for running the program's main logic.
+ *
+ * Return:      Returns an integer indicating the exit status of the program
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
