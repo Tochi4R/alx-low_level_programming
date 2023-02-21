38d6384334ef219fc85075e2e503bb24febdbe25
Samuel_tochukwu <tochukwu_samuel@yahoo.com>
2023-02-21 13:40:22 -0800

    main.h file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void print_hello();
+int add_numbers(int a, int b);
+
+#define PI 3.14159
+
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+#endif /* MAIN_H */
