6d890112e5fc611b95c0eb4a5b3335d548719792
Samuel_tochukwu <tochukwu_samuel@yahoo.com>
2023-02-24 15:51:21 -0800

    Collaboration is multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -7,36 +7,5 @@
  */
 int mul(int a, int b)
 {
-	int product = a * b;
-	int sign = 1;
-
-	if (product < 0)
-	{
-		sign = -1;
-		product = -product;
-	}
-	if (product == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-
-	int digits[10];
-	int num_digits = 0;
-
-	while (product > 0)
-	{
-		digits[num_digits] = product % 10;
-		product /= 10;
-		num_digits++;
-	}
-	if (sign == -1)
-	{
-		_putchar('-');
-	}
-	for (int i = num_digits - 1; i >= 0; i--)
-	{
-		_putchar(digits[i] + '0');
-	}
 	return (a * b);
 }
