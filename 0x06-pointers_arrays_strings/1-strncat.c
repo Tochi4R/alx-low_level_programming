5ef9be9c0516dd0f9cccc2333c6723d84f249a63
Samuel_tochukwu <tochukwu_samuel@yahoo.com>
2023-03-01 13:41:38 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,7 +18,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[dest_len + 1] = src[i];
 
-	dest[dest_len + 1] != '\0';
+	dest[dest_len + 1] = '\0';
 
 	return (dest);
 }
