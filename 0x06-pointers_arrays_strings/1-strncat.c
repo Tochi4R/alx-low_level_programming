942afbd13453ad4686d84dc1e07c23f266746c5c
Samuel_tochukwu <tochukwu_samuel@yahoo.com>
2023-03-01 14:11:33 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _strncat - Concatinate two values
  *
@@ -12,13 +13,22 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len = strlen(dest);
 	int i;
+	int j;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[dest_len + 1] = src[i];
+	j = 0;
+	i = 0;
 
-	dest[dest_len + 1] = '\0';
-
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
