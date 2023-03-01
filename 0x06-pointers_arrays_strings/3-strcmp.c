6cfd5310077da92105f219563c05638f3dc2ab74
Samuel_tochukwu <tochukwu_samuel@yahoo.com>
2023-03-01 14:47:10 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,12 +12,14 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] < s2[i]) ? -1 : 1;
+			return (s1[i] - s2[i]);
 		}
+		i++;
 	}
-	return (s1[i] == s2[i]) ? 0 : (s1[i] < s2[i]) ? -1 : 1;
+	return (0);
 }
