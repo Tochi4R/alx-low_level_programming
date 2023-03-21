af143286145f4e9b02eadf9190582f592f5cc686
Samuel_tochukwu <tochukwu_samuel@yahoo.com>
2023-03-21 14:31:31 -0700

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,9 +2,11 @@
 /**
  * main - A program that print the name of file
  * it was compiled from
+ *
+ * Return: 0
  */
 int main(void)
 {
-	printf("Compiled from file: %s\n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
