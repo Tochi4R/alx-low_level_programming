e33c73a1de56977e975d39a3d3956e2a612aa6ff
Tochi4R <tochukwu_samuel@yahoo.com>
2023-04-08 19:55:42 -0700

    2. 10

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,11 +8,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int mask = 1;
+
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
-	unsigned long int mask = 1;
 
 	mask <<= index;
 	return ((n & mask) != 0);
