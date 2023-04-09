dc2f5f72f9d4d2603a3415cd7f048641f88ad13c
Tochi4R <tochukwu_samuel@yahoo.com>
2023-04-08 19:26:32 -0700

    0.0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,13 +9,14 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int result = 0;
+	int i;
+
 	if (b == NULL)
 	{
 		return (0);
 	}
-	unsigned int result = 0;
-
-	for (int i = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		char c = b[i];
 
